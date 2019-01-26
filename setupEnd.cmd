e63eec9b0854bafe1ef6b80893d1d3ccce20cd03
BuildTools <unwarned@unknown.spigot.mc>
2019-01-26 17:34:03 +0200

    #39 Reuploading repository
    Reuploading repository through broken lite breaks
    Part I: Removing all files

--- setupEnd.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "skip=1 tokens=1,2,*" %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do (
-  del /q "%%k\AdVirC.lnk">nul 2>nul
-  call "%~dp0\subroutines\modules\shortcut.exe" /a:c /f:"%%k\AdVirC.lnk" /t:"%~dp0\starter.cmd" /i:"%~dp0\design\icon.ico">nul 2>nul
-)
