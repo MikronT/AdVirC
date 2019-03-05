e8e89789af4e17d344c35f89f4fe6d689a897422
BuildTools <unwarned@unknown.spigot.mc>
2019-03-05 18:37:56 +0200

    #104 Updated update.cmd

--- subroutines/update.cmd
+++ subroutines/update.cmd
@@ -13,6 +13,9 @@ for /f "tokens=1,2,* delims=- " %%i in ("%*") do (
 
 if "%key_check%" == "true" (
   set key_check=false
+
+  if "%setting_update_databases_auto%" == "true" ( call :checkVersion databases ) else if "%setting_update_databases_remind%" == "true" call :checkVersion databases
+  if "%setting_update_program_auto%" == "true"   ( call :checkVersion program   ) else if "%setting_update_program_remind%" == "true"   call :checkVersion program
 )
 
 
@@ -24,4 +27,20 @@ if "%key_check%" == "true" (
 if "%key_update%" == "true" (
   set key_update=false
 )
+exit /b
+
+
+
+
+
+
+
+
+
+:checkVersion
+if "%1" == "databases" %module_wget% "%update_databases_versionUrl%" --output-document=%update_databasesVersion_output%
+if "%1" == "program"   %module_wget% "%update_program_versionUrl%"   --output-document=%update_programVersion_output%
+
+for /f "delims=" %%i in (%update_databasesVersion_output%) do rem
+for /f "delims=" %%i in (%update_programVersion_output%) do rem
 exit /b
