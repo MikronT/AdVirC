d2d0fe038b7b37f6398168baa11a8ec240587c98
MikronT <45333177+MikronT@users.noreply.github.com>
2019-04-22 20:32:31 +0300

    Duplicate bug fix

--- subroutines/cleaning.cmd
+++ subroutines/cleaning.cmd
@@ -57,13 +57,15 @@ echo.
 echo.%language_cleaning_exceptionsRewriting%
 
 for %%i in (%cleaning_extensions% %cleaning_files% %cleaning_folders% %cleaning_processes% %cleaning_registry% %cleaning_services% %cleaning_shortcuts% %cleaning_tasks% %cleaning_temp%) do (
-  copy /y %%i %%i.old>>%log_debug%
-  del /q %%i
+  for /f "eol=# delims=" %%j in (%dataDir%\settings\exceptions.db) do (
+    copy /y %%i %%i.old>>%log_debug%
+    del /q %%i
 
-  for /f "eol=# delims=" %%j in (%dataDir%\settings\exceptions.db) do for /f "eol=- delims=" %%l in ('find /i /v "%%j" %%i.old') do echo.%%k>>%%i
+    for /f "eol=- delims=" %%k in ('find /i /v "%%j" %%i.old') do echo.%%k>>%%i
 
-  del /q %%i.old
-)>nul 2 >nul
+    del /q %%i.old
+  )
+)>nul 2>nul
 %loadingUpdate% 3 force
 
 
