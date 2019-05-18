b959db0a9e5a2f99ba7846372480451a5d34a09b
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-18 15:52:10 +0300

    Updated experimental backupping

--- subroutines/cleaning/deleting.cmd
+++ subroutines/cleaning/deleting.cmd
@@ -85,7 +85,8 @@ if exist %cleaning_processes% for /f "delims=" %%i in (%cleaning_processes%) do 
 if exist %cleaning_registry% for /f "delims=" %%i in (%cleaning_registry%) do (
   set errorLevel=
   if "%cleaningRule_experimental%" == "true" (
-    reg export %%i %dataDir%\backups\registry_%currentDate%.reg /y>>%log_debug%
+    reg export %%i temp\registryBackupBuffer.reg /y>>%log_debug%
+    for /f "delims=" %%r in (temp\registryBackupBuffer.reg) do echo.%%r>>%dataDir%\backups\registry_%currentDate%.reg
   )
   reg delete %%i /f>>%log_debug%
   if "!errorLevel!" == "0" (
