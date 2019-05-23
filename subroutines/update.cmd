b10566e45bef9341cc08287021d7a5236525d37d
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-23 22:36:38 +0300

    Removed old code

--- subroutines/update.cmd
+++ subroutines/update.cmd
@@ -13,54 +13,16 @@ for /f "tokens=1 delims=- " %%i in ("%*") do if "%%i" NEQ "" set %%i
 
 
 
-if "%key_check%" == "databases" (
-  %module_wget% "%update_databases_version_url%" --output-document=%update_databases_version_output%
-
-  for /f "tokens=1-3 delims= " %%i in (%update_databases_version_output%) do (
-    if /i "%%i" == "%setting_update_channel%" (
-      for /f "tokens=1-8 delims=." %%l in ("%%j") do (
-               if "%%l" NEQ "" if %%l GTR %databases_version_code_level1% ( echo.>temp\return_update_databases_available
-        ) else if "%%m" NEQ "" if %%m GTR %databases_version_code_level2% ( echo.>temp\return_update_databases_available
-        ) else if "%%n" NEQ "" if %%n GTR %databases_version_code_level3% ( echo.>temp\return_update_databases_available
-        ) else if "%%o" NEQ "" if %%o GTR %databases_version_code_level4% ( echo.>temp\return_update_databases_available
-        ) else if "%%p" NEQ "" if %%p GTR %databases_version_code_level5% ( echo.>temp\return_update_databases_available
-        ) else if "%%q" NEQ "" if %%q GTR %databases_version_code_level6% ( echo.>temp\return_update_databases_available
-        ) else if "%%r" NEQ "" if %%r GTR %databases_version_code_level7% ( echo.>temp\return_update_databases_available
-        ) else if "%%s" NEQ "" if %%s GTR %databases_version_code_level8% ( echo.>temp\return_update_databases_available
-        )
-      )
-    )
-  )
-)
-
-if "%key_update%" == "databases" if exist temp\return_update_databases_available call subroutines\main.cmd :databases_update
-
-
-
-
-
-
-
 if "%key_check%" == "program" (
   %module_wget% "%update_program_version_url%" --output-document=%update_program_version_output%
 
   for /f "tokens=1-3 delims= " %%i in (%update_program_version_output%) do (
     if /i "%%i" == "%setting_update_channel%" (
       for /f "tokens=1-7 delims=." %%l in ("%%j") do (
-               if "%%l" NEQ "" if %%l GTR %program_version_code_level1% ( echo.>temp\return_update_program_available
-        ) else if "%%m" NEQ "" if %%m GTR %program_version_code_level2% ( echo.>temp\return_update_program_available
-        ) else if "%%n" NEQ "" if %%n GTR %program_version_code_level3% ( echo.>temp\return_update_program_available
-        ) else if "%%o" NEQ "" if %%o GTR %program_version_code_level4% ( echo.>temp\return_update_program_available
-        ) else if "%%p" NEQ "" if %%p GTR %program_version_code_level5% ( echo.>temp\return_update_program_available
-        ) else if "%%q" NEQ "" if %%q GTR %program_version_code_level6% ( echo.>temp\return_update_program_available
-        ) else if "%%r" NEQ "" if %%r GTR %program_version_code_level7% ( echo.>temp\return_update_program_available
-        )
       )
     )
   )
 )
-
-setlocal EnableDelayedExpansion
 
 if "%key_update%" == "program" if exist temp\return_update_program_available (
   if exist "%temp%\%program_name%-Update" rd /s /q "%temp%\%program_name%-Update"
