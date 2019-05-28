df7659de3b2e67b6f3491467011b70d60ef442d2
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-28 23:06:04 +0300

    Heuristic rules default state shange

--- subroutines/main.cmd
+++ subroutines/main.cmd
@@ -22,7 +22,7 @@ set module_unZip=subroutines\modules\unzip.exe -qq
 set module_wget=subroutines\modules\wget.exe --quiet --no-check-certificate --tries=1
 
 set setting_cleaningRule_experimental=false
-set setting_cleaningRule_heuristic=true
+set setting_cleaningRule_heuristic=false
 set setting_debug=false
 set setting_firstRun=true
 set setting_language=default
