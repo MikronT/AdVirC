a4c3ae78cb06fdae128ba3fd19e7be24175a6617
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-30 17:20:05 +0300

    A lot of fixes for the uninstaller

--- uninstall.cmd
+++ uninstall.cmd
@@ -2,16 +2,16 @@
 chcp 65001>nul
 
 %~d0
-cd %~dp0
+cd "%~dp0"
 
 set program_name=AdVirC
-set program_version_name=%program_name% v2.0 Pre-Alpha [MikronT]
+set program_version_name=%program_name% v2.0 Beta 1 Nightly 1 [MikronT]
 
 set dataDir=data
 set uninstallDirectory=%cd%
 
-if "%setting_appearance_logo:~0,1%" == "" set setting_appearance_logo=Ш
 if "%setting_appearance_logo%"      == "" set setting_appearance_logo=Ш
+if "%setting_appearance_theme%"     == "" set setting_appearance_theme=0b
 
 for /f %%i in ('"prompt $h & echo on & for %%j in (1) do rem"') do set input_backspace=%%i
 for /f "skip=2 tokens=3,* delims= " %%i in ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ReleaseId') do set windowsVersionID=%%i
@@ -22,13 +22,19 @@ for /f "skip=2 tokens=3,* delims= " %%i in ('reg query "HKLM\SOFTWARE\Microsoft\
 
 :uninstallQuestion
 set command=command
-if exist subroutines\methods.cmd call subroutines\methods.cmd :logo main
-
+if exist subroutines\methods.cmd (
+  call subroutines\methods.cmd :logo main
+) else (
+  color %setting_appearance_theme%
+  echo.
+  echo.  %program_name% Uninstaller
+  echo.
+)
 echo.  ^(i^) Uninstall directory: %uninstallDirectory%
 echo.
-echo.  ^(^?^) Do you want to uninstall %program_name%^?
-echo.      ^(1^) Uninstall
 echo.
+echo.  ^(^?^) Do you want to uninstall %program_name%^?
+echo.      ^(#^) Uninstall
 echo.      ^(0^) Cancel
 echo.
 echo.
@@ -41,16 +47,16 @@ if "%windowsVersionID%" NEQ "" (
 ) else set /p command=%input_backspace%   ^(^>^) Enter the command ^> 
 
 if "%command%" == "0" ( set command= & exit /b )
-if "%command%" NEQ "1" goto :uninstallQuestion
+if "%command%" NEQ "#" goto :uninstallQuestion
 
 
 
 if exist subroutines\methods.cmd call subroutines\methods.cmd :loadingUpdate stop
 timeout /nobreak /t 1 >nul
 
-if exist %dataDir%\backups\consoleSettingsBackup.reg reg import %dataDir%\backups\consoleSettingsBackup.reg 2>nul
+if exist %dataDir%\backups\consoleSettings.reg reg import %dataDir%\backups\consoleSettings.reg 2>nul
 
-for /f "skip=2 tokens=2,* delims= " %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do set location_desktop=%%j
+for /f "skip=2 tokens=2,* delims= " %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do call set location_desktop=%%j
 if exist "%location_desktop%\%program_name%.lnk" del /q "%location_desktop%\%program_name%.lnk"
 
 
