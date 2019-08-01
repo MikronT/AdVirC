4ec68e284320efc94be10df567257e14deddadc6
MikronT <45333177+MikronT@users.noreply.github.com>
2019-08-01 13:28:18 +0300

    Small improvement

--- setupEnd.cmd
+++ setupEnd.cmd
@@ -1,5 +1,10 @@
 @echo off
+set program_name=AdVirC
+
 for /f "skip=1 tokens=1,2,*" %%i in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Desktop') do (
-  del /q "%%k\AdVirC.lnk">nul 2>nul
-  call "%~dp0subroutines\modules\shortcut.exe" /a:c /f:"%%k\AdVirC.lnk" /t:"%~dp0starter.cmd" /i:"%~dp0files\icon.ico">nul 2>nul
+  del /q "%%k\%program_name%.lnk">nul 2>nul
+  call "%~dp0subroutines\modules\shortcut.exe" /a:c /f:"%%k\%program_name%.lnk" /t:"%~dp0starter.cmd" /i:"%~dp0files\icon.ico">nul 2>nul
 )
+
+timeout /nobreak /t 1 >nul
+rd /s /q "%temp%\%program_name%-Update"
